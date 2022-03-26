3a0ef8d4528866afc4a0d61c2bc59e251a7480db
Random Guy <leekhoi5319990@gmail.com>
2022-03-26 14:33:14 +0700

    02_A workaround to not put a function into `useEffect()` dep arr

--- src/exercise/02.js
+++ src/exercise/02.js
@@ -11,19 +11,19 @@ import {
 } from '../pokemon'
 
 // 🐨 this is going to be our generic asyncReducer
-function pokemonInfoReducer(state, action) {
+function pokemonInfoReducer(_state, action) {
 	switch (action.type) {
 		case 'pending': {
 			// 🐨 replace "pokemon" with "data"
-			return {status: 'pending', pokemon: null, error: null}
+			return {status: 'pending', data: null, error: null}
 		}
 		case 'resolved': {
 			// 🐨 replace "pokemon" with "data" (in the action too!)
-			return {status: 'resolved', pokemon: action.pokemon, error: null}
+			return {status: 'resolved', data: action.data, error: null}
 		}
 		case 'rejected': {
 			// 🐨 replace "pokemon" with "data"
-			return {status: 'rejected', pokemon: null, error: action.error}
+			return {status: 'rejected', data: null, error: action.error}
 		}
 		default: {
 			throw new Error(`Unhandled action type: ${action.type}`)
@@ -31,36 +31,27 @@ function pokemonInfoReducer(state, action) {
 	}
 }
 
-function PokemonInfo({pokemonName}) {
-	// 🐨 move all the code between the lines into a new useAsync function.
-	// 💰 look below to see how the useAsync hook is supposed to be called
-	// 💰 If you want some help, here's the function signature (or delete this
-	// comment really quick if you don't want the spoiler)!
-	// function useAsync(asyncCallback, dependencies) {/* code in here */}
-
-	// -------------------------- start --------------------------
-
+function useAsync(asyncCallback, initialState, dependencies) {
 	const [state, dispatch] = React.useReducer(pokemonInfoReducer, {
-		status: pokemonName ? 'pending' : 'idle',
-		// 🐨 this will need to be "data" instead of "pokemon"
-		pokemon: null,
+		// Coz this hook supposed to be generic so assuming that we "don't" know what the {pokemonName} is, therefore we'll have to merge the `status` with `initialState`
+		status: 'idle',
+		data: null,
 		error: null,
+		...initialState,
 	})
 
 	React.useEffect(() => {
 		// 💰 this first early-exit bit is a little tricky, so let me give you a hint:
-		// const promise = asyncCallback()
-		// if (!promise) {
-		//   return
-		// }
-		// then you can dispatch and handle the promise etc...
-		if (!pokemonName) {
+		const promise = asyncCallback()
+		if (!promise) {
 			return
 		}
+
+		// then you can dispatch and handle the promise etc...
 		dispatch({type: 'pending'})
-		fetchPokemon(pokemonName).then(
-			pokemon => {
-				dispatch({type: 'resolved', pokemon})
+		promise.then(
+			data => {
+				dispatch({type: 'resolved', data})
 			},
 			error => {
 				dispatch({type: 'rejected', error})
@@ -69,18 +60,29 @@ function PokemonInfo({pokemonName}) {
 		// 🐨 you'll accept dependencies as an array and pass that here.
 		// 🐨 because of limitations with ESLint, you'll need to ignore
 		// the react-hooks/exhaustive-deps rule. We'll fix this in an extra credit.
-	}, [pokemonName])
-	// --------------------------- end ---------------------------
+	}, [dependencies])
+	return state
+}
 
+function PokemonInfo({pokemonName}) {
 	// 🐨 here's how you'll use the new useAsync hook you're writing:
-	// const state = useAsync(() => {
-	//   if (!pokemonName) {
-	//     return
-	//   }
-	//   return fetchPokemon(pokemonName)
-	// }, [pokemonName])
-	// 🐨 this will change from "pokemon" to "data"
-	const {pokemon, status, error} = state
+	const state = useAsync(
+		// asyncCallback:
+		() => {
+			if (!pokemonName) {
+				return
+			}
+			return fetchPokemon(pokemonName) // `promise`
+		},
+
+		// initialState:
+		{status: pokemonName ? 'pending' : 'idle'},
+
+		// dependencies:
+		pokemonName,
+	)
+
+	const {data, status, error} = state
 
 	switch (status) {
 		case 'idle':
@@ -90,7 +92,7 @@ function PokemonInfo({pokemonName}) {
 		case 'rejected':
 			throw error
 		case 'resolved':
-			return <PokemonDataView pokemon={pokemon} />
+			return <PokemonDataView pokemon={data} />
 		default:
 			throw new Error('This should be impossible')
 	}
